9fc1bb3b3c49683755b68389212bca06743efc7d
Bagus W <baguswicak@gmail.com>
2020-10-22 09:03:57 +0700

    First Commit

---
+++ src/views/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    <HeaderShanay/>
+    <HeroShayna/>
+    <WomenShayna/>
+    <InstaShayna/>
+    <PartnerShayna/>
+    <FooterShayna/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import HeaderShanay from '@/components/HeaderShanay.vue';
+import HeroShayna from '@/components/HeroShayna.vue';
+import WomenShayna from '@/components/WomenShayna.vue';
+import InstaShayna from '@/components/InstaShayna.vue';
+import PartnerShayna from '@/components/PartnerShayna.vue';
+import FooterShayna from '@/components/FooterShayna.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    HeaderShanay,    
+    HeroShayna,
+    WomenShayna,
+    InstaShayna,
+    PartnerShayna,
+    FooterShayna
+  }
+}
+</script>
